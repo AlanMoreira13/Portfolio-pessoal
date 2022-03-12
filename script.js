658cbd8a46e7d802db70633291dac649503a063f
Alan Moreira <moreiraalan22@gmail.com>
2022-03-12 11:51:46 -0300

    Meu Portfolio

---
+++ script.js
@@ -0,0 +1,45 @@
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("show-menu");
+
+});
+
+// Regras de orçamento:
+
+// Valor por página: R$100,00
+// Script:           10% do custo das página
+// Layout:           R$500,00
+// Taxa de Urgência: 10% do custo total por semana de urgência
+
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluilayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1;
+    if (incluilayout) preco += 500;
+    let taxaUrgencia = 1 - prazo * 0.1;
+    preco *= 1 + taxaUrgencia
+
+
+    document.querySelector("#preco").innerHTML = "R$ " + preco.toFixed(2)
+}
+
+
+
+
